827c3c6e12c7580269b52e5e31c4df8dea1e53e8
Adam Turner <aturner5278@gmail.com>
2024-09-08 17:10:25 +0100

    Refactored main to be cleaner

---
+++ src/CLIProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Library.src;
+public class CLIProgram
+{
+
+    private string dbPath;
+	public CLIProgram(string path)
+	{
+        dbPath = path;
+    }
+
+    public void Run()
+    {
+        ReadDatabase readDatabase = new(dbPath);
+        var running = true;
+        var account = readDatabase.GetAccount(1);
+
+        if (account == null)
+        {
+            Console.WriteLine("Account does not exist");
+            Environment.Exit(1);
+        }
+
+        CLI app = new CLI(account, dbPath);
+        app.DisplayUI();
+
+        do
+        {
+            var result = app.UpdateUI();
+
+            if (result == -1)
+            {
+                running = false;
+            }
+        } while (running);
+
+        //readDatabase.SaveData(account);
+    }
+}
